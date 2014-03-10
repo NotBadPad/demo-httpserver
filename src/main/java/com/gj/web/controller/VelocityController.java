6b4059904e5d296a17554645621652ddc9d6aa9f
good-temper <awayy1432@163.com>
2014-03-10 11:45:44 +0800

    Velocity整合测试

---
+++ src/main/java/com/gj/web/controller/VelocityController.java
@@ -0,0 +1,29 @@
+package com.gj.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eh.core.model.ResultInfo;
+import org.eh.core.web.controller.Controller;
+
+/**
+ *
+ * @author guojing
+ * @date 2014-3-10
+ */
+@org.eh.core.annotation.Controller(name = "velocity", url = "/test/velocity")
+public class VelocityController implements Controller {
+
+	@Override
+	public ResultInfo process(Map<String, Object> parms) {
+		ResultInfo resultInfo = new ResultInfo();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", parms.get("name"));
+		map.put("msg", parms.get("msg"));
+
+		resultInfo.setResultMap(map);
+		resultInfo.setView("myvm");
+		return resultInfo;
+	}
+
+}
