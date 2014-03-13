4eba723a9339e9314844ceae8b9c53b98ff3a6cd
good-temper <awayy1432@163.com>
2014-03-13 10:41:14 +0800

    测试easy-httpserver修改

--- src/test/java/com/gj/web/other/UtilTest.java
+++ src/test/java/com/gj/web/other/UtilTest.java
@@ -1,6 +1,6 @@
 package com.gj.web.other;
 
-import org.eh.core.util.FileUtil;
+import org.eh.core.util.IOUtil;
 import org.junit.Test;
 
 /**
@@ -12,7 +12,7 @@ public class UtilTest {
 
 	@Test
 	public void testReadImg() {
-		byte[] bytes = FileUtil
+		byte[] bytes = IOUtil
 				.readFileByBytes("E:/mywork/test-httpserver/target/classes/static/pic/C_3.png");
 		System.out.println(bytes.length);
 	}
